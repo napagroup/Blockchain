8950be620a27c698b8f915e4127de32c8e1efca3
John Echelon <johshin@gmail.com>
2016-09-01 12:55:35 -0400

    Updated chart to display plot data on hover.

--- client/scripts/bitCoin/bitCoinController.js
+++ client/scripts/bitCoin/bitCoinController.js
@@ -27,7 +27,6 @@
             var getChartData = function(){
                   
                 $scope.onClick = function (points, evt) {
-                console.log(points, evt);
                 };
                 $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
                 $scope.options = {
@@ -54,18 +53,12 @@
                         var values = data.values;
                         var xValues = [];
                         for(var i = 0; i< values.length; i++){
-                            $scope.labels.push( (new Date(values[i].x)).toDateString());
+                            //$scope.labels.push( (new Date(values[i].x)).toDateString());
+                            $scope.labels.push(moment.unix(values[i].x).format('hh:mm:ss'));
                             xValues.push(values[i].y);
                         }
                         $scope.data.push(xValues);
-
-                        //$scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
                         $scope.series = ['Market Price (USD)'];
-                        /*$scope.data = [
-                            [65, 59, 80, 81, 56, 55, 40],
-                            [28, 48, 40, 19, 86, 27, 90]
-                        ];*/
-                                       
                     },function(err){
                 });
 
